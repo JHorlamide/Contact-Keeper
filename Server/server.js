e7d4c74460c00a69440bec3c4f6dfd06caa09378
JHorlamide <olamidejubril68@gmail.com>
2020-08-25 13:58:11 +0100

    Database configuration and connection

--- Server/server.js
+++ Server/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const connectDB = require('./Config/db');
 
 const app = express();
 
@@ -7,6 +8,8 @@ const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
   res.json({msg: 'Welcome to the ContactKeeper API...'});
 });
+/*** ConnectDB ***/ 
+connectDB();
 
 /*** Define Route ***/ 
 app.use('/api/auth', require('./Routes/auth'));
